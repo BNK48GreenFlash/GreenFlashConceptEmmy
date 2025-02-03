9005e0d65d420aa9dc8e7df86792c32db5c0509c
BNK48GreenFlash <kalosrsxy@gmail.com>
2025-02-03 09:05:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function tweetMessage() {
-    const message = "Emmy GreenFlash\n#EmmyBNK48 #BNK48\n#GreenFlashConceptDay1\n#BNK4818thSINGLE";
+    const message = "Emmy GreenFlash\n#EmmyBNK48\n#GreenFlashConceptDay1";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }
